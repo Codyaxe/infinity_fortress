83948eea8cbaaa16997d328d5d09338243fc8e31
Codyaxe <vincelorzano@gmail.com>
2025-10-29 18:00:03 +0800

    feat: test structure

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,27 @@
 public class Game {
     public static void main(String[] args) {
+        Game game = new Game();
+        game.intro();
+    }
 
+    public void intro() {
+        Thread t = new Thread(() -> {
+            printtype(
+                    "Welcome to the infinity fortress! The game that manifests abominations beyond your imaginations",
+                    50);
+        });
+        t.start();
+    }
+
+    public void printtype(String s, long duration) {
+        // A new thread that detects enter will be included here!
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i));
+            try {
+                Thread.sleep(duration);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
