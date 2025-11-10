c8778caa308b06fbcebf8db0f83a3f6b2b662143
Codyaxe <vincelorzano@gmail.com>
2025-11-11 00:21:22 +0800

    chore: added comment

--- src/main/java/com/infinityfortress/Character.java
+++ src/main/java/com/infinityfortress/Character.java
@@ -1,12 +1,14 @@
 package com.infinityfortress;
 
 import java.util.Random;
+// Will be removed
 
 public class Character {
 
     public enum Type {
         ALLY, ENEMY
     };
+
     public boolean isEmpty = false;
     public String role;
     public String race;
@@ -29,40 +31,40 @@ public class Character {
 
     // Equipment arrays for randomization
     private static final String[] WEAPONS = {
-        "Iron Sword", "Steel Blade", "Mystic Staff", "War Hammer", "Elven Bow",
-        "Dagger", "Crossbow", "Magic Wand", "Battle Axe", "Holy Mace"
+            "Iron Sword", "Steel Blade", "Mystic Staff", "War Hammer", "Elven Bow",
+            "Dagger", "Crossbow", "Magic Wand", "Battle Axe", "Holy Mace"
     };
 
     private static final String[] ARMOR = {
-        "Leather Armor", "Chain Mail", "Plate Armor", "Robes", "Scale Mail",
-        "Studded Leather", "Ring Mail", "Splint Armor", "Hide Armor", "Padded Armor"
+            "Leather Armor", "Chain Mail", "Plate Armor", "Robes", "Scale Mail",
+            "Studded Leather", "Ring Mail", "Splint Armor", "Hide Armor", "Padded Armor"
     };
 
     private static final String[] ACCESSORIES = {
-        "Ring of Power", "Amulet of Health", "Cloak of Protection", "Belt of Strength",
-        "Boots of Speed", "Gloves of Dexterity", "Crown of Wisdom", "Shield of Faith",
-        "Bracers of Defense", "Necklace of Magic", "Ring of Luck", "Charm of Fortune"
+            "Ring of Power", "Amulet of Health", "Cloak of Protection", "Belt of Strength",
+            "Boots of Speed", "Gloves of Dexterity", "Crown of Wisdom", "Shield of Faith",
+            "Bracers of Defense", "Necklace of Magic", "Ring of Luck", "Charm of Fortune"
     };
 
     private static final String[] ROLES = {
-        "Mage", "Warlock", "Cleric", "Summoner", "Rogue", "Archer", "Healer", "Tank", "Warrior"
+            "Mage", "Warlock", "Cleric", "Summoner", "Rogue", "Archer", "Healer", "Tank", "Warrior"
     };
 
     private static final String[] RACES = {
-        "Beastkin", "Demon", "Dragonborn", "Dwarf", "Elf", "Human", "Nephilim", "Orc", "Spiritborn"
+            "Beastkin", "Demon", "Dragonborn", "Dwarf", "Elf", "Human", "Nephilim", "Orc", "Spiritborn"
     };
 
     public Character(String type) {
         Random rand = new Random();
-        this.hp = this.maxHp = 50 + rand.nextInt(200);     // 50-249
-        this.mp = this.maxMp = 30 + rand.nextInt(170);       // 30-199
-        this.expCap = rand.nextInt(100);             // 0-99
-        this.defense = 10 + rand.nextInt(90);     // 10-99
-        this.strength = 15 + rand.nextInt(85);    // 15-99
-        this.speed = 10 + rand.nextInt(90);       // 10-99
-        this.critChance = 5 + rand.nextInt(45);   // 5-49
+        this.hp = this.maxHp = 50 + rand.nextInt(200); // 50-249
+        this.mp = this.maxMp = 30 + rand.nextInt(170); // 30-199
+        this.expCap = rand.nextInt(100); // 0-99
+        this.defense = 10 + rand.nextInt(90); // 10-99
+        this.strength = 15 + rand.nextInt(85); // 15-99
+        this.speed = 10 + rand.nextInt(90); // 10-99
+        this.critChance = 5 + rand.nextInt(45); // 5-49
         this.critStrength = 10 + rand.nextInt(40); // 10-49
-        this.luck = 1 + rand.nextInt(99);         // 1-99
+        this.luck = 1 + rand.nextInt(99); // 1-99
 
         this.type = type.equals("Ally") ? Type.ALLY : Type.ENEMY;
         // Randomized role and race (ignore parameters, randomize instead)
